02eab07536b0d8e0cabc4bafd132ccb7921de10c
RPsaila <RPsaila@users.noreply.github.com>
2019-02-12 19:43:53 -0500

    Added changes to Script Score for new mechanic
    Added mechanic that adds balls whenever multiplier hits 3 and deletes them whenever multiplier goes back to normal.
    Added comments

---
+++ Pinball_RPsaila/Assets/Scripts/Score.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Score : MonoBehaviour
+{
+    // Created a list to help deal with all the added balls and their destruction
+
+    List<GameObject> balls = new List<GameObject>();
+
+    public TMPro.TextMeshPro multipliertext;
+    public GameObject theball;
+
+    public int maxBalls = 3;
+    public GameObject ballPrefab;
+
+    int score = 0;
+    int multiplier = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    public void FixedUpdate()
+    {
+        this.GetComponent<TMPro.TextMeshPro>().text = score.ToString();
+        multipliertext.text = "x" + multiplier.ToString();
+
+        // if multiplier is equal to x3, it will add two balls
+
+        if (multiplier == 3 && balls.Count < maxBalls)
+        {
+        GameObject ball = (GameObject)Instantiate(ballPrefab);
+        balls.Add(ball);
+        }
+
+        // when multiplier is back to normal, balls disappear
+
+        else if (multiplier < 3)
+        {
+            foreach (GameObject ball in balls)
+            {
+                Destroy(ball);
+            }
+            balls.Clear();
+        }
+    }
+
+    public void AddScore(int points)
+    {
+        score = score + points * multiplier;
+    }
+
+    public void Addmultiplier(int multiplierpoints)
+    {
+        multiplier = multiplier + multiplierpoints;
+    }
+
+    public void ResetScore()
+    {
+        score = 0;
+    }
+
+    public void ResetMultiplier()
+    {
+        multiplier = 1;
+    }
+}
